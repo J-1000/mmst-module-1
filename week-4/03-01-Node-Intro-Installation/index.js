fa56c28b0f9a609d9b22c08b41f2a37f24b4de32
Jan Redmann <janredmann1@gmail.com>
2019-12-10 10:50:08 +0100

    add node intro

---
+++ week-4/03-01-Node-Intro-Installation/index.js
@@ -0,0 +1,8 @@
+const kreide = require('chalk');
+
+// console.log(kreide.bgCyan('Text mit Kreide'));
+
+const supervillains = require('supervillains');
+
+const allVillains = supervillains.all;
+console.log(allVillains);
