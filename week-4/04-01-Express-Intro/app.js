96ebe4e284550ae1a1df4eb919e5894ed8bf587f
Jan Redmann <janredmann1@gmail.com>
2019-12-10 14:45:11 +0100

    add express intro

--- week-4/04-01-Express-Intro/app.js
+++ week-4/04-01-Express-Intro/app.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/eine-datei.html', (request, response) => {
+    response.send(` <p>wenn im public folder keine resource gefunden wird, dann antwortet die erste passende Route die definiert wurde</p>
+    `
+    );
+});
+
+app.get('/cat', (request, response) => {
+    response.sendFile(__dirname + '/views/cat-page.html')
+});
+
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
