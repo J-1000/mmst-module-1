a9e6a12f28dd43288744b2949a5d1308ececfc16
MarcoSantonastasi <marco.santonastasi@gmail.com>
2019-12-12 16:48:44 +0100

    Lesson material

--- week-4/04-10-ExpressGET-Params-Query/app.js
+++ week-4/04-10-ExpressGET-Params-Query/app.js
@@ -6,8 +6,12 @@ const hbs     = require('hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-  console.log(req);
+app.get('/', function (req, res) { // send the form to the user
+  res.render("index");
+})
+
+app.get('/search', function (req, res) { // responds to the received form
+  res.send(req.query);
 })
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
