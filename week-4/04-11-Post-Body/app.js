89059127ae736749867a52a8b819314995f31402
MarcoSantonastasi <marco.santonastasi@gmail.com>
2019-12-12 17:35:11 +0100

    Lesson finished

--- week-4/04-11-Post-Body/app.js
+++ week-4/04-11-Post-Body/app.js
@@ -2,16 +2,27 @@ const express = require('express');
 const app     = express();
 const path    = require('path');
 const hbs     = require('hbs'); 
+const bodyparser = require("body-parser");
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // sends the form to the user
   res.render('user-info-form');
 })
 
-app.get('/display-user-info', (req, res) => {
-  
+app.post('/display-user-info', (req, res) => {
+  let name      = req.body.name;
+  let age       = req.body.age;
+  let superhero = req.body.superhero;
+
+  res.render("index", {htm:`<p>
+    Your name is ${name}
+    Your age is ${age}
+    Your favorite superhero is ${superhero}
+  </p>`})
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
