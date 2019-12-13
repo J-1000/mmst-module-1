f126fd65d7a7585c4564bc766ddcba63b2e2090e
Jan Redmann <janredmann1@gmail.com>
2019-12-13 19:15:07 +0100

    add lab express spotify

--- week-4/05-08-LAB-Mongoose-Recipes/solution-with-promisall.js
+++ week-4/05-08-LAB-Mongoose-Recipes/solution-with-promisall.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
