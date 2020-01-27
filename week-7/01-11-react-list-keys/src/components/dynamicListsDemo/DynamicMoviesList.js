0c0d72ccfe0458cd844475e4855e401117fdb056
Jan Redmann <janredmann1@gmail.com>
2020-01-27 10:13:53 +0100

    add conditional rendering

--- week-7/01-11-react-list-keys/src/components/dynamicListsDemo/DynamicMoviesList.js
+++ week-7/01-11-react-list-keys/src/components/dynamicListsDemo/DynamicMoviesList.js
@@ -6,24 +6,17 @@ class DynamicMoviesList extends Component {
     super();
     this.state = {
       movies: [
-        {
-          _id: new mongoose.Types.ObjectId(),
-          title: "The Godfather",
-          director: "Francis Coppola"
-        },
-        {
-          _id: new mongoose.Types.ObjectId(),
-          title: "Star Wars",
-          director: "Rian Johnson"
-        },
-        {
-          _id: new mongoose.Types.ObjectId(),
-          title: "The Shawshank Redemption",
-          director: "Frank Darabont"
-        }
-      ]
+        { _id: new mongoose.Types.ObjectId(), title: "The Godfather", director: "Francis Coppola", hasOscars: true, IMDbRating: 9.2 },
+        { _id: new mongoose.Types.ObjectId(), title: "Star Wars", director: "Rian Johnson", hasOscars: true, IMDbRating: 8.7 },
+        { _id: new mongoose.Types.ObjectId(), title: "The Shawshank Redemption", director: "Frank Darabont", hasOscars: false, IMDbRating: 9.3 }
+      ],
+      showOscarAwarded: false // <== add
     };
   }
+  toggleMovies = () => {
+    this.setState({ showOscarAwarded: !this.state.showOscarAwarded })
+  }
+
   deleteMovieHandler = movieId => {
     console.log("Delete Movie with Id:" + movieId);
     this.setState({
@@ -31,15 +24,33 @@ class DynamicMoviesList extends Component {
     });
   };
 
+  addHarryPotter = () => {
+    const moviesCopy = [...this.state.movies]; // <== notice the spread operator here!
+    moviesCopy.push({ _id: new mongoose.Types.ObjectId(), title: "Harry Potter", director: "No idea" });
+    this.setState({
+      movies: moviesCopy
+    })
+  }
+
   render() {
     // leave this console.log() so we can keep track of our state inside our browser's console
     console.log(this.state.movies);
+    const showOscarAwarded = this.state.showOscarAwarded;
+    const filteredMovies = this.state.movies.filter(theMovie => theMovie.hasOscars === showOscarAwarded);
+    const filteredMoviesJSX = filteredMovies.map(
+      (oneMovie) => {
+        return <ImprovedCard key={oneMovie._id} {...oneMovie} clickToDelete={this.deleteMovieHandler} />
+      }
+    )
+
     return (
       <div>
-        {this.state.movies.map(oneMovie => {
-          //return <ImprovedCard key={oneMovie._id} _id={oneMovie._id} title={oneMovie.title} director={oneMovie.director} />
-          return <ImprovedCard key={oneMovie._id} {...oneMovie} clickToDelete={() => this.deleteMovieHandler(oneMovie._id)}/>;
-        })}
+        {filteredMoviesJSX}
+        <button onClick={this.addHarryPotter}>Add Harry Potter</button>
+        <button onClick={this.toggleMovies}>
+          {this.state.showOscarAwarded ? 'Hide Oscar Awarded' : 'Show Oscar Awarded'}
+        </button>
+
       </div>
     );
   }
--- week-7/01-11-react-list-keys/src/components/dynamicListsDemo/ImprovedCard.js
+++ week-7/01-11-react-list-keys/src/components/dynamicListsDemo/ImprovedCard.js
@@ -1,10 +1,22 @@
 import React from 'react';
 const improvedCard = (props) => {
+  let awardText;
+  if (props.hasOscars) {
+    if (props.IMDbRating >= 9) {
+      awardText = <p> WOW! Oscar Award and IMDb rating {props.IMDbRating}! </p>
+    } else if (props.IMDbRating >= 7) {
+      awardText = <p> Got the Oscar Award and has IMDb rating {props.IMDbRating}, not bad at all! </p>
+    }
+  } else {
+    awardText = <p> Has IMDb rating {props.IMDbRating}. </p>
+  }
   return (
-    <div  className="movies-list-item">
-        <h2>{props.title}</h2>
-        <p>Director: {props.director}</p>
-        <button onClick={props.clickToDelete}>Delete</button>
+    <div className="movies-list-item">
+      <h2>{props.title}</h2>
+      <p>Director: {props.director}</p>
+      {awardText}
+      <button onClick={props.clickToDelete}>Delete</button>
+
     </div>
   )
 };
